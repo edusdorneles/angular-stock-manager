8a3db3e3c889b596af89bbbe7494adbefb65d8e5
dududornelees <eduardo.dsdorneles@gmail.com>
2023-09-03 00:17:14 -0300

    init: início do projeto

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-stock-manager';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.primengConfig.zIndex = { modal: 1100, overlay: 1000, menu: 1000, tooltip: 1100 };
+  }
+}
